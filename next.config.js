58347cfd5247e6653f2b416e6e642108fc4e7150
HanSeongLee <jkhanseong@gmail.com>
2025-01-01 17:04:41 +0900

    fix(next.config): fix next.config.js

--- next.config.js
+++ next.config.js
@@ -2,30 +2,29 @@ const { withSentryConfig } = require('@sentry/nextjs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  },
-  // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'api.ts', 'api.js'],
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-  },
-}
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        });
+        return config;
+    },
+    // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'api.ts', 'api.js'],
+    sentry: {
+        // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+        // for client-side builds. (This will be the default starting in
+        // `@sentry/nextjs` version 8.0.0.) See
+        // https://webpack.js.org/configuration/devtool/ and
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+        // for more information.
+        hideSourceMaps: true,
+    },
+};
 
 module.exports = withSentryConfig(
     nextConfig,
@@ -37,8 +36,7 @@ module.exports = withSentryConfig(
       silent: true,
 
       dryRun: process.env.NODE_ENV !== 'production',
-    },
-    {
+
       // For all available options, see:
       // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
